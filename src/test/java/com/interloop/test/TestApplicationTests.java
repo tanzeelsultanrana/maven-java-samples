6e86c2360ea46e01af57c10e60ea248d898f95a6
TANZEEL SULTAN RANA <57807565+tanzeelsultanrana@users.noreply.github.com>
2023-03-08 10:52:56 +0500

    added annotation

--- src/test/java/com/interloop/test/TestApplicationTests.java
+++ src/test/java/com/interloop/test/TestApplicationTests.java
@@ -1,9 +1,11 @@
 package com.interloop.test;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootApplication
 class TestApplicationTests {
 
 	@Test
